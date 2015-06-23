72a7e951546a5277b2549bd448ef98fccb108a4f
katatunix <katatunix@gmail.com>
2015-06-23 11:16:10 +0700

    Update

--- source/gereport/editor/EditorHtml.php
+++ source/gereport/editor/EditorHtml.php
@@ -1,10 +1,8 @@
 <script src="<?= $this->config->resDirUrl() ?>ckeditor/ckeditor.js"></script>
 <script>
-	$(function() {
-		CKEDITOR.replace('<?= $this->id ?>', {
-			'extraPlugins' : 'autogrow,colorbutton,colordialog',
-			'autoGrow_bottomSpace' : 20,
-			'autoGrow_onStartup' : true
-		});
+	CKEDITOR.replace('<?= $this->id ?>', {
+		'extraPlugins' : 'autogrow,colorbutton,colordialog',
+		'autoGrow_bottomSpace' : 20,
+		'autoGrow_onStartup' : true
 	});
 </script>
